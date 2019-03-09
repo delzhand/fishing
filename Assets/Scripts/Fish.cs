45b226531e2deed652cbe01a2124127bc16fda44
Derek Adams <derek@alloymagnetic.com>
2019-03-09 14:16:28 -0500

    initial commit

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public enum FishType
+{
+    crappie,
+    bass,
+    drum,
+    sunfish,
+    bluegill,
+    catfish,
+    trout
+}
+
+public class Fish : MonoBehaviour
+{
+    public float currentStamina;
+    public int maxStamina;
+    public int size;
+    public FishType fishType;
+    public float speed = 1;
+
+    void InitFish()
+    {
+        Array values = Enum.GetValues(typeof(FishType));
+        Random random = new Random();
+        this.fishType = (FishType)values.GetValue(random.Next(values.Length));
+        this.size = random.Next(5, 10);
+        this.maxStamina = size * 25;
+        this.currentStamina = this.maxStamina;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitFish();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class FishMovement : MonoBehaviour
+{
+
+    public bool isMoving = false;
+    public float timer = 0;
+    public float idleDuration = 3;
+    public float movingDuration = 1;
+    public Vector2 moveDirection = Vector2.zero;
+    private Random random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        random = new Random();
+        moveDirection = new Vector2(random.Next(1), random.Next(1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > idleDuration)
+        {
+            isMoving = true;
+            gameObject.transform.localPosition += new Vector3(moveDirection.x, 0, moveDirection.y) * Time.deltaTime;
+
+        }
+        if (timer > (idleDuration + movingDuration))
+        {
+            isMoving = false;
+            timer -= (idleDuration + movingDuration);
+            moveDirection = new Vector2(random.Next(1), random.Next(1));
+        }
+    }
+}
